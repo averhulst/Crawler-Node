aea7be8bb35115bab8f525e40d51cd7ed9a6073f
Andy <enuary@gmail.com>
2016-05-28 16:23:30 -0700

    Crawler node now does a cpu lookup to determine how many worker threads to run

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -5,7 +5,11 @@ import org.apache.log4j.BasicConfigurator;
 
 public class Main {
     public static void main(String[] args){
-        //int crawlerThreadCount = Integer.parseInt(args[0]);
+        int availableCpus = Runtime.getRuntime().availableProcessors();
+        int crawlerWorkerThreadCount = availableCpus * 2 + 1;
+
+        System.out.println(availableCpus + " processors found, using " + crawlerWorkerThreadCount + " worker threads for crawling");
+
         BasicConfigurator.configure();
         Crawler crawler = new Crawler(2);
         crawler.crawl();
